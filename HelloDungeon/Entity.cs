af97227c0809f9c98d30e84150dd7844d0962b3f
Devin Broussard <devin.ray.broussard@gmail.com>
2021-08-18 10:01:32 -0500

    update fighting

--- HelloDungeon/Entity.cs
+++ HelloDungeon/Entity.cs
@@ -8,18 +8,23 @@ namespace HelloDungeon
     {
         public string name;
         private int hp;
-        public int Health 
+        public int Health
         {
             get { return hp; }
         }
         public int attack;
         public bool isTurn;
-        public Entity(string Name, int Hp, int Attack, bool IsTurn)
+        public int critHit;
+        public int defense;
+
+        public Entity(string Name, int Hp, int Attack, bool IsTurn, int CritHit, int Defense)
         {
             name = Name;
             hp = Hp;
             attack = Attack;
             isTurn = IsTurn;
+            critHit = CritHit;
+            defense = Defense;
         }
 
         public int takeDamage(int damageAmount)
@@ -36,6 +41,16 @@ namespace HelloDungeon
         public bool isAlive()
         {
             return hp > 0;
+        }
+
+        public void Fight(Entity otherEntity)
+        {
+            otherEntity.takeDamage(attack);
+            Console.WriteLine($"{name} attacks the {otherEntity.name} for {attack} damage!");
+            Console.WriteLine($"{otherEntity.name}'s new Hp: {otherEntity.Health}");
+            Console.ReadLine();
+            isTurn = false;
+            otherEntity.isTurn = true;
         }
     }
 }
